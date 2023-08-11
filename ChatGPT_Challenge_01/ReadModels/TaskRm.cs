a8954471f8028413a25ea204d55421ec4f4281cd
Davy Gevaert <davy.gevaert@student.vives.be>
2023-08-11 18:51:34 +0200

    Add project files.

---
+++ ChatGPT_Challenge_01/ReadModels/TaskRm.cs
@@ -0,0 +1,6 @@
+﻿namespace ChatGPT_Challenge_01.ReadModels
+{
+    // shorter version instead of getters and setters
+    // record is introduced in C# 9
+    public record TaskRm(Guid Id, string Title, string Description, DateTime DueDate, DateTime Completed);
+}
