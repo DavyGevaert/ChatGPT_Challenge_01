a52ae3438398de074196c7cf769ba68740b84499
Davy Gevaert <davy.gevaert@student.vives.be>
2023-08-13 09:16:10 +0200

    DateTime now correctly accepted in create-task.component added hyperlink to go to swagger web api

--- ChatGPT_Challenge_01/ClientApp/src/app/create-task/create-task.component.ts
+++ ChatGPT_Challenge_01/ClientApp/src/app/create-task/create-task.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { FormBuilder } from '@angular/forms'
 import { Task } from '../model/task';
-import { Guid } from 'guid-typescript';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
   styleUrls: ['./create-task.component.css']
 })
-export class CreateTaskComponent implements OnInit {
+export class CreateTaskComponent {
 
   task: Task = {};
 
   constructor(private apiService: ApiService,
-              private fb: FormBuilder) {
+              private fb: FormBuilder,
+              private router: Router) {
 
   }
 
@@ -23,12 +24,8 @@ export class CreateTaskComponent implements OnInit {
     title: [''],
     description: [''],
     dueDate: [''],
-    completed: [''],
+    completed: ['']
   })
-
-  ngOnInit(): void {
-    
-  }
 
   postTask() {
     console.log("Form Values:", this.form.value);
@@ -36,9 +33,18 @@ export class CreateTaskComponent implements OnInit {
     this.task.id = this.form.value.id ?? 'not loaded';
     this.task.title = this.form.value.title ?? 'not loaded';
     this.task.description = this.form.value.description ?? 'not loaded';
-    this.task.dueDate = new Date();
-    this.task.completed = new Date();
+
+    // add default minutes part
+    let dueDate = this.form.value.dueDate?.toString() + ':00';
+    this.task.dueDate = new Date(dueDate);
+
+    // add default minutes part
+    let completed = this.form.value.completed?.toString() + ':00';
+    this.task.completed = new Date(completed);
 
     this.apiService.createTask(this.task).subscribe(_ => console.log("form posted to server"));
+
+    // return to homepage after task is created
+    this.router.navigate(['/search-tasks'])
   }
 }
