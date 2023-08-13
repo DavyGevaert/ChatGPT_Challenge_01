149455d299677568bfac4d68d9546e4e1dc9bda0
Davy Gevaert <davy.gevaert@student.vives.be>
2023-08-13 20:17:26 +0200

    edit details, api service put method to edit task

--- ChatGPT_Challenge_01/ClientApp/src/app/details-task/details-task.component.ts
+++ ChatGPT_Challenge_01/ClientApp/src/app/details-task/details-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'
 import { ApiService } from '../services/api.service';
 import { Task } from '../model/task';
+import { FormBuilder } from '@angular/forms'
 
 
 @Component({
@@ -17,8 +18,17 @@ export class DetailsTaskComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private router: Router,
+              private fb: FormBuilder,
               private apiService: ApiService) {
   }
+
+  form = this.fb.group({
+    id: [''],
+    title: [''],
+    description: [''],
+    dueDate: [''],
+    completed: ['']
+  })
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(p => this.findTask(p.get("Id")));
@@ -43,6 +53,25 @@ export class DetailsTaskComponent implements OnInit {
     console.log("Response Error. Status: ", err.status)
     console.log("Response Error. Status Text: ", err.statusText)
     console.log(err)
+  }
+
+  editTask() {
+    console.log("Form Values:", this.form.value);
+
+    this.task.id = this.taskId;
+    this.task.title = this.form.value.title ?? 'not loaded';
+    this.task.description = this.form.value.description ?? 'not loaded';
+
+    // add default minutes part
+    let dueDate = this.form.value.dueDate?.toString() + ':00';
+    this.task.dueDate = new Date(dueDate);
+
+    this.task.completed = !!(this.form.value.completed ?? false);  // something is wrong here, as input boolean in edit screen is not accepted into web api
+
+    this.apiService.editTask(this.taskId, this.task).subscribe(_ => console.log("form posted to server"));
+
+    // return to homepage after task is created
+    this.router.navigate(['/search-tasks'])
   }
 
 }
--- ChatGPT_Challenge_01/ClientApp/src/app/services/api.service.ts
+++ ChatGPT_Challenge_01/ClientApp/src/app/services/api.service.ts
@@ -10,16 +10,24 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
+  // GET ALL
   public getListTasks(): Observable<Task[]> {
     return this.httpClient.get<Task[]>('https://localhost:7025/api/Tasks')
   }
 
+  // GET BY ID
   // id is string in JavaScript json
   public getTaskById(id: string): Observable<Task> {
     return this.httpClient.get<Task>('https://localhost:7025/api/Tasks/' + id)
   }
 
+  // POST
   public createTask(task: Task): Observable<Task> {
     return this.httpClient.post<Task>('https://localhost:7025/api/Tasks/', task)
+  }
+
+  // PUT
+  public editTask(guid: string, task: Task): Observable<Task> {
+    return this.httpClient.put<Task>('https://localhost:7025/api/Tasks/' + guid, task)
   }
 }
